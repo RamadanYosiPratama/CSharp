6175d416d09038e80f3309fd0731c19269e12ca5
RamadanYosiPratama <ramadanyosi30@gmail.com>
2020-01-30 10:31:38 +0700

    CShrap basic

---
+++ Basic/Assets/Latihan3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int c = jumlah(50, 50);
+        Debug.Log("Hasil dari A + B adlah:" + c);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    int jumlah(int a, int b){
+        return a + b;
+    }
+}
---
+++ Basic/Assets/Latihan6.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan6 : MonoBehaviour
+{
+
+    int[] intArray;//Deklarasikan array terlebih dahulu
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        intArray = new int[4];//inisialisasi Array
+        intArray[0] = 10;//Memasukkan Nilai Array
+        intArray[1] = 20;        
+        intArray[2] = 30;
+        intArray[3] = 40;
+
+        Debug.Log("Menampilkan Semua Array");
+        foreach (int a in intArray) {
+            Debug.Log(a);
+        }
+        Debug.Log("Nilai Index ke dua adalah" + intArray[2]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
